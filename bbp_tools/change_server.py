48fd6506f1c411a854432a8c1e61a522149f7d53
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2020-02-04 16:49:54 +0100

    Make a decent modulue out of it. We can now call it from other python code

--- bbp_tools/change_server.py
+++ bbp_tools/change_server.py
@@ -5,22 +5,30 @@
 import sys
 import lxml.etree as ET
 
-if len(sys.argv) != 4:
-    print('Expected 3 arguments: <src project> <target project> <new address>')
-    sys.exit(-1)
 
-src_file = sys.argv[1]
-target_file = sys.argv[2]
-new_server_address = sys.argv[3]
+def change_server(src_file, target_file, new_server_address):
 
-try:
-    with open(src_file, 'r') as f:
-        tree = ET.parse(f)
-except:
-    print("Can't read '%s'. Is this a ByteBlower GUI project?" % src_file)
+    try:
+        with open(src_file, 'r') as f:
+            tree = ET.parse(f)
+    except:
+        print("Can't read '%s'. Is this a ByteBlower GUI project?" % src_file)
 
-for port in tree.iterfind('ByteBlowerGuiPort'):
-    for config in port.iterfind('ByteBlowerGuiPortConfiguration'):
-        config.attrib['physicalServerAddress'] = new_server_address
+    for port in tree.iterfind('ByteBlowerGuiPort'):
+        for config in port.iterfind('ByteBlowerGuiPortConfiguration'):
+            config.attrib['physicalServerAddress'] = new_server_address
 
-tree.write(target_file)    
+    tree.write(target_file)
+
+
+if '__main__' == __name__:
+    if len(sys.argv) != 4:
+        print(
+            'Expected 3 arguments: <src project> <target project> <new address>'
+        )
+        sys.exit(-1)
+    src_file = sys.argv[1]
+    target_file = sys.argv[2]
+    new_server_address = sys.argv[3]
+
+    change_server(src_file, target_file, new_server_address)
